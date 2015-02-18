847c56add3715c445d5f01f2d869f4c441bc9a08
Bryan Finlayson <bdfinlayson@gmail.com>
2015-02-17 22:05:59 -0600

    added code to take user input for number of desired objects on page. included remove button too

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
-var html = '';
+var circles = ' ';
 var red;
 var green;
 var blue;
 var rgbColor;
+var $button = $('#submitButton');
+var $input = $('#userInput');
+var $remove = $('#removeButton');
+var i;
+var circleNum;
 
-for ( var i = 1; i <= 100; i += 1) {
-  red = Math.floor(Math.random() * 256 );
-  green = Math.floor(Math.random() * 256 );
-  blue = Math.floor(Math.random() * 256 );
-  rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
-  html += '<div style="background-color:' + rgbColor + '"></div>';  
-}
+$($remove).on('click', function (evt) {
+  $('.circles').remove();
+})
 
-document.write(html);
+$($button).on('click', function (evt) {
+
+  evt.preventDefault();
+  circleNum = $input.val();
+
+  for ( i = 1; i <= circleNum; i++) {
+    red = Math.floor(Math.random() * 256 );
+    green = Math.floor(Math.random() * 256 );
+    blue = Math.floor(Math.random() * 256 );
+    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+    circles += '<div class="circles" style="background-color:' + rgbColor + '"></div>';
+
+  }
+  $('.circles').remove();
+  $('p').append(circles);
+})
+
